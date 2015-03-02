47547c0aaeaf15d87c7ec90565ee1b96fc8b1bed
Jon Hall <jonathan.hall@webappuk.com>
2015-03-02 17:56:41 +0000

    (WIP) Stashing some progress on JsDictionary, ran into an issue with interfaces having InlineCode while trying to utilize explicit interface implementation and need to do some further work on this.

--- Runtime/CoreLib.TestScript/Collections/Generic/GenericJsDictionaryTests.cs
+++ Runtime/CoreLib.TestScript/Collections/Generic/GenericJsDictionaryTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using QUnit;
 
 namespace CoreLib.TestScript.Collections.Generic {
@@ -79,6 +80,83 @@ namespace CoreLib.TestScript.Collections.Generic {
 			var d = JsDictionary<string, object>.GetDictionary(new { a = "valueA", b = 134 });
 			d.Remove("a");
 			Assert.AreEqual(d.Keys, new[] { "b" });
+		}
+
+		[Test]
+		public void CastToDictionaryWorks()
+		{
+			JsDictionary<string, object> d = JsDictionary<string, object>.GetDictionary(new { a = "valueA", b = 134 });
+			Dictionary<string, object> d2 = (Dictionary<string, object>) d;
+			
+			// Check all keys and values copied over ok
+			foreach (var key in d.Keys) {
+				Assert.AreEqual(d[key], d2[key]);
+			}
+
+			// Use a dictionary-specific method as additional confirmation that the conversion went according to plan
+			object val;
+			Assert.IsFalse(d2.TryGetValue("c", out val));
+			Assert.IsNull(val);
+		}
+
+		[Test]
+		public void CastFromDictionaryWorks()
+		{
+			Dictionary<string, object> d = new Dictionary<string, object>() {{"a", "valueA"}, {"b", 134}};
+			JsDictionary<string, object> d2 = (JsDictionary<string, object>)d;
+
+			// Check all keys and values copied over ok
+			foreach (var key in d.Keys)
+			{
+				Assert.AreEqual(d[key], d2[key]);
+			}
+
+			// Verify our jsdictionary only has its own keys
+			Assert.AreEqual(Keys(d2), new[] {"a", "b"});
+		}
+
+		[Test]
+		public void AddKvpWorks()
+		{
+			var d = new JsDictionary<string, object>();
+			d.Add(new KeyValuePair<string, object>("a", 55));
+			d.Add(new KeyValuePair<string, object>("b", "valueB"));
+			Assert.AreEqual(d["a"], 55);
+			Assert.AreEqual(d["b"], "valueB");
+		}
+
+		[Test]
+		public void AddWorks()
+		{
+			var d = new JsDictionary<string, object>();
+			d.Add("a", 55);
+			d.Add("b", "valueB");
+			Assert.AreEqual(d["a"], 55);
+			Assert.AreEqual(d["b"], "valueB");
+		}
+
+		[Test]
+		public void CollectionInitializerWorks()
+		{
+			var d = new JsDictionary<string, object>() {{"a", 55}, {"b", "valueB"}};
+			Assert.AreEqual(d["a"], 55);
+			Assert.AreEqual(d["b"], "valueB");
+		}
+
+		[Test]
+		public void ContainsWorks()
+		{
+			ICollection<KeyValuePair<string,object>> d = new JsDictionary<string, object>() { { "a", 55 }, { "b", "valueB" } };
+			Assert.IsTrue(d.Contains(new KeyValuePair<string, object>("b", "valueB")));
+			Assert.IsFalse(d.Contains(new KeyValuePair<string, object>("b", "valueA")));
+		}
+
+		[Test]
+		public void ThisIsBroked()
+		{
+			IX x = new X();
+			x.Do();
+			((X) x).Do();
 		}
 	}
 }
--- Runtime/CoreLib/Collections/Generic/JsDictionary.cs
+++ Runtime/CoreLib/Collections/Generic/JsDictionary.cs
@@ -13,40 +13,28 @@ namespace System.Collections.Generic {
 	[IgnoreNamespace]
 	[IncludeGenericArguments(false)]
 	[ScriptName("Object")]
-	public sealed class JsDictionary<TKey, TValue> {
+	public sealed class JsDictionary<TKey, TValue> : ICollection<KeyValuePair<TKey, TValue>> {
 		[InlineCode("{{}}")]
-		public JsDictionary() {
-		}
+		public JsDictionary() {}
 
-		public JsDictionary(params object[] nameValuePairs) {
-		}
+		public JsDictionary(params object[] nameValuePairs) {}
 
 		public int Count {
-			[InlineCode("{$System.Script}.getKeyCount({this})")]
-			get {
-				return 0;
-			}
+			[InlineCode("{$System.Script}.getKeyCount({this})")] get { return 0; }
 		}
 
 		public new ICollection<TKey> Keys {
-			[InlineCode("{$System.Object}.keys({this})")]
-			get {
-				return null;
-			}
+			[InlineCode("{$System.Object}.keys({this})")] get { return null; }
 		}
 
 		[IntrinsicProperty]
 		public TValue this[TKey key] {
-			get {
-				return default(TValue);
-			}
-			set {
-			}
+			get { return default(TValue); }
+			set { }
 		}
 
 		[InlineCode("{$System.Script}.clearKeys({this})")]
-		public void Clear() {
-		}
+		public void Clear() {}
 
 		[InlineCode("{$System.Script}.keyExists({this}, {key})")]
 		public bool ContainsKey(TKey key) {
@@ -64,7 +52,53 @@ namespace System.Collections.Generic {
 		}
 
 		[InlineCode("delete {this}[{key}]")]
-		public void Remove(TKey key) {
+		public void Remove(TKey key) {}
+
+		[InlineCode("new ({$System.Script}.makeGenericType({$System.Collections.Generic.Dictionary`2}, [{TKey}, {TValue}]))({d})")]
+		public static explicit operator Dictionary<TKey, TValue>(JsDictionary<TKey, TValue> d) {
+			return null;
 		}
+
+		[InlineCode("(function(){{var $t1={{}},$t2={d}.getEnumerator(),$t3;try{{while($t2.moveNext()){{$t3=$t2.current();$t1[$t3.key]=$t3.value;}}}}finally{{$t2.dispose();}} return $t1;}})()")]
+		public static explicit operator JsDictionary<TKey, TValue>(Dictionary<TKey, TValue> d)
+		{
+			return null;
+		}
+
+		[InlineCode("{this}[{item}.key]={item}.value")]
+		public void Add(KeyValuePair<TKey, TValue> item) {}
+
+
+		[InlineCode("{this}[{key}]={value}")]
+		public void Add(TKey key, TValue value) { }
+
+		[InlineCode("{$System.Script}.equals({this}[{item}.key], {item}.value)")]
+		bool ICollection<KeyValuePair<TKey, TValue>>.Contains(KeyValuePair<TKey, TValue> item) {
+			return false;
+		}
+
+		// TODO: If value at key equals supplied value then remove the key
+		bool ICollection<KeyValuePair<TKey, TValue>>.Remove(KeyValuePair<TKey, TValue> item) {
+			return false;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() {
+			return GetEnumerator();
+		}
+
+		IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator() {
+			return GetEnumerator();
+		}
+	}
+
+	public class X : IX {
+		[InlineCode("{this}.notDo()")]
+		public string Do() {
+			return "a";
+		}
+	}
+
+	public interface IX {
+		string Do();
 	}
 }
